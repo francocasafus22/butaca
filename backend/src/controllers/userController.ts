10d5b861d7a9f92cc9051e0f1e52aee208357e9d
francocasafus22 <francocasafus55@gmail.com>
2025-07-02 13:51:26 -0300

    add : login function

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { User } from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { hashPassword, checkPassword } from "../utils/auth";
+import { generateJWT } from "../utils/jwt";
 import slug from "slug";
 
 export const getUser = async (req: Request, res: Response) => {
@@ -19,9 +20,11 @@ export const createAccount = async (req: Request, res: Response) => {
       return;
     }
 
+    // remove spaces and capital letters
     const username = slug(req.body.username, "");
     const usernameExist = await User.findOne({ username });
 
+    // check if the username is used
     if (usernameExist) {
       const error = new Error("El user ya está en uso, pruebe con otro.");
       res.status(400).json({ error: error.message });
@@ -29,6 +32,7 @@ export const createAccount = async (req: Request, res: Response) => {
     }
 
     const user = new User(req.body);
+
     user.password = await hashPassword(password);
     user.username = username;
 
@@ -37,6 +41,38 @@ export const createAccount = async (req: Request, res: Response) => {
     res.status(201).json({ message: "Usuario creado" });
   } catch (error) {
     console.error("Error al registrar: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    // check if the email is registered
+    if (!user) {
+      const error = new Error("No hay ningun user con ese mail.");
+      res.status(400).json({ error: error.message });
+      return;
+    }
+
+    const isPasswordCorrect = await checkPassword(password, user.password);
+
+    // check if the hash password is correct
+    if (!isPasswordCorrect) {
+      const error = new Error("La contraseña no es correcta");
+      res.status(400).json({ error: error.message });
+      return;
+    }
+
+    // generate the jwt
+    const token = generateJWT({ id: user._id });
+
+    res.send(token);
+  } catch (error) {
+    console.error("Error al iniciar sesion:", error.message);
     res.status(500).json({ error: error.message });
   }
 };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createAccount, getUser } from "../controllers/userController";
+import { createAccount, getUser, login } from "../controllers/userController";
 
 const UserRoutes = Router();
 
 UserRoutes.get("/", getUser);
 
 UserRoutes.post("/register", createAccount);
+
+UserRoutes.post("/login", login);
 
 export default UserRoutes;
