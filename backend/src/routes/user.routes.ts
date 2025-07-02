ea96569a96665d13531b6145d99a9ad9aacb2396
francocasafus22 <francocasafus55@gmail.com>
2025-07-02 14:06:04 -0300

    add: express validator to login and register

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,12 +1,32 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount, getUser, login } from "../controllers/userController";
+import { handleInputErrors } from "../middlewares/validation";
 
 const UserRoutes = Router();
 
 UserRoutes.get("/", getUser);
 
-UserRoutes.post("/register", createAccount);
+UserRoutes.post(
+  "/register",
 
-UserRoutes.post("/login", login);
+  body("username").notEmpty().withMessage("User obligatorio"),
+  body("email").notEmpty().withMessage("Email obligatorio"),
+  body("password")
+    .isStrongPassword()
+    .withMessage(
+      "La contraseña debe tener mas de 8 caractere, una mayuscula, un numero y un simbolo"
+    ),
+  handleInputErrors,
+  createAccount
+);
+
+UserRoutes.post(
+  "/login",
+  body("email").notEmpty().withMessage("Email obligatorio."),
+  body("password").notEmpty().withMessage("Contraseña obligatoria."),
+  handleInputErrors,
+  login
+);
 
 export default UserRoutes;
